45d63de9743c82a546878dfbd008850837878061
William Konitzer <william@konitzer.co.uk>
2023-10-02 12:14:06 +0100

    more pylint fixes

--- fetch_functions.py
+++ fetch_functions.py
@@ -1,3 +1,8 @@
+"""
+This module contains functions to fetch the latest release information for
+specified products from different types of repositories and registries.
+"""
+
 import re
 from datetime import datetime
 import importlib
@@ -8,13 +13,23 @@ from bs4 import BeautifulSoup
 
 
 def fetch_mcr(product_config):
+    """
+    Fetch and return the latest release information for MCR.
+
+    :param product_config: Configuration dictionary containing details about
+    the product such as 'repository', 'channel', and 'component'.
+    :type product_config: dict
+    :return: A list of dictionaries each containing 'name' and 'date' of a
+             release.
+    :rtype: list of dict
+    """    
     config = importlib.import_module('config')
     config.logger.debug('fetch_mcr called with configuration: %s',
                         product_config)
 
     repository = product_config.get('repository')
-    channel = product_config.get('channel', 'stable')
-    component = product_config.get('component', 'docker')
+    channel = product_config.get('channel')
+    component = product_config.get('component')
     url = f"{repository}/win/static/{channel}/x86_64/"
 
     config.logger.debug('Constructed URL: %s', url)
@@ -22,7 +37,7 @@ def fetch_mcr(product_config):
     try:
         response = requests.get(url, timeout=5)
         response.raise_for_status()
-    
+
         config.logger.debug('HTTP response status: %s', response.status_code)
         config.logger.debug('HTTP response text: %s', response.text)
 
@@ -46,21 +61,51 @@ def fetch_mcr(product_config):
 
         return releases
 
-    except requests.RequestException as e:
-        config.logger.error('HTTP request failed: %s', e)
+    except requests.RequestException as request_exception:
+        config.logger.error('HTTP request failed: %s', request_exception)
         return []
-    except Exception as e:
-        config.logger.error("An unexpected error occurred: %s", e)
+    except ValueError as value_error:
+        config.logger.error("An unexpected error occurred: %s", value_error)
         return []
 
 
 def fetch_mke(product_config):
+    """
+    Fetches the latest release information for the specified MKE product from
+    Docker Hub's registry.
+
+    This function constructs a URL based on the provided configuration and
+    sends an HTTP GET request to it. The response is then parsed to extract
+    release information, which is returned as a list of dictionaries, each
+    containing the name and date of a release.
+
+    Parameters:
+        product_config (dict): A dictionary containing the configuration for
+            the product, which includes 'repository' and optionally 'registry'.
+            Example:
+                {
+                    'repository': 'mirantis/ucp',
+                    'registry': 'https://hub.docker.com'  # optional
+                }
+
+    Returns:
+        list: A list of dictionaries containing the name and date of each
+        release. If an error occurs or no releases are found, returns an empty
+        list.
+        Example:
+            [{'name': '3.4.5',
+             'date': datetime.datetime(2023, 10, 1, 12, 0, 0)}]
+
+    Raises:
+        ValueError: If required keys are missing in the product_config.
+        requests.RequestException: For issues related to the HTTP request.
+    """    
     config = importlib.import_module('config')
     config.logger.debug('fetch_mke called with configuration: %s',
                         product_config)
 
     repository = product_config.get('repository')
-    registry = product_config.get('registry', 'https://hub.docker.com')
+    registry = product_config.get('registry')
     url = f"{registry}/v2/repositories/{repository}/tags"
 
     config.logger.debug('Constructed URL: %s', url)
@@ -91,20 +136,67 @@ def fetch_mke(product_config):
 
         return releases
 
-    except (requests.RequestException, requests.HTTPError) as e:
-        config.logger.error("Error fetching %s: %s", url, e)
+    except (requests.RequestException, requests.HTTPError) as request_error:
+        config.logger.error("Error fetching %s: %s", url, request_error)
         return []
-    except Exception as e:
-        config.logger.error("An unexpected error occurred: %s", e)
+    except ValueError as value_error:
+        config.logger.error("Value error occurred: %s", value_error)
         return []
 
 def fetch_msr(product_config):
+    """
+    Fetches and parses the latest release information for a specified MSR
+    product from Mirantis registry or a Helm chart repository, based on the
+    provided configuration.
+    
+    The function constructs the request URL from the configuration, sends an
+    HTTP GET request to retrieve the release information, and then parses the
+    response to extract the relevant details. It returns a list containing
+    dictionaries with the name and date of each release found.
+    
+    Parameters:
+        product_config (dict): A configuration dictionary specifying the
+            details about the product whose release information is to be
+            fetched. It should contain keys such as 'repository', 'registry',
+            and 'branch'.
+            Example:
+                {
+                    'repository': 'msr/msr',
+                    'registry': 'https://registry.mirantis.com',  # optional, 
+                                                         specifies registry URL
+                    'branch': '3.1'  # specifies the branch of the product
+                }
+    
+    Returns:
+        list: A list of dictionaries, each representing a release with its
+            name and date.
+            Example:
+                [
+                    {'name': '3.4.5', 'date': datetime.datetime(2023, 10, 1,
+                     12, 0, 0)}
+                ]
+            Returns an empty list if no releases are found or an error occurs 
+            during the fetch operation.
+    
+    Raises:
+        ValueError: If required keys in the product_config are missing.
+        requests.RequestException: If there are issues related to the HTTP 
+        request, e.g. connectivity problems, timeout, etc.
+        yaml.YAMLError: If there is an error in parsing the YAML response from 
+        the Helm chart repository.
+    
+    Notes:
+        The function handles fetching from different sources (registry or Helm
+        chart repository) based on the branch version specified in the
+        product_config. It filters the fetched releases based on the specified
+        branch and logs errors and warnings for various failure scenarios.
+    """    
     config = importlib.import_module('config')
     config.logger.debug('fetch_msr called with configuration: %s',
                         product_config)
 
     repository = product_config.get('repository')
-    registry = product_config.get('registry', 'https://registry.mirantis.com')
+    registry = product_config.get('registry')
     branch = product_config.get('branch')
 
     config.logger.debug('Repository: %s, Registry: %s, Branch: %s',
@@ -115,11 +207,11 @@ def fetch_msr(product_config):
     if branch_major and branch_major >= 3:
         url = f"{registry}/charts/{repository}/index.yaml"
         config.logger.debug('Constructed URL: %s', url)
-        
+
         try:
             response = requests.get(url, timeout=5)
             response.raise_for_status()
-            
+
             config.logger.debug('HTTP response status: %s',
                                 response.status_code)
             config.logger.debug('HTTP response text: %s', response.text)
